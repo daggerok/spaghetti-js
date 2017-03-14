93f574dbe9876401c22c4527a0c2405cded3ed88
Maksim Kostromin <daggerok@gmail.com>
2017-03-14 13:15:16 +0200

    add comments

--- src/main.js
+++ src/main.js
@@ -4,22 +4,24 @@ require('normalize.css/normalize.css');
 require('./styles.css');
 
 const obj = {
-  num: 42,
+  num: 1,
 };
 
 const app = document.querySelector('#app');
 var result;
 
-const func = function func(n) {
-  return this.num + n;
+const func = function func(a, b) {
+  return this.num + a, b;
 };
 
-// 1
-// result = func.apply(obj, [1]);
+// 1: apply requires array, returns result
+result = func.apply(obj, [1, 2]);
 
-// 2
-// result = func.call(obj, 2);
+// 2: call requires sequence of args, returns result
+result = func.call(obj, 2, 3);
 
-// 3
-result = func.bind(obj, 3)();
+// 3: bind , returns a fiction, which we need to call
+const bound = func.bind(obj, 3, 4);
+result = bound();
+
 app.innerHTML = result;
