a3d459383c516db1f50fb3b77170c2a4f2188adb
Maksim Kostromin <daggerok@gmail.com>
2017-03-14 13:06:13 +0200

    implement apply; call and bind usage

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
+// 'use strict';
+
+require('normalize.css/normalize.css');
+require('./styles.css');
+
+const obj = {
+  num: 42,
+};
+
 const app = document.querySelector('#app');
-app.innerHTML = 'hi!';
+var result;
+
+const func = function func(n) {
+  return this.num + n;
+};
+
+// 1
+// result = func.apply(obj, [1]);
+
+// 2
+// result = func.call(obj, 2);
+
+// 3
+result = func.bind(obj, 3)();
+app.innerHTML = result;
